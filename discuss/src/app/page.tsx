ecadb41f581a407fe2e1d8ad451978119fe711e4
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-10-03 20:38:55 +0545

    add header component and authentication

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,13 +1,7 @@
-
-import { Button } from "@/components/ui/button";
-import { signIn } from "./actions/sign-in";
-import { signOut } from "./actions/sign-out";
-
 export default async function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      
     </div>
   );
 }
