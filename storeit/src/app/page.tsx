02292e70e37549438daa017f7b6887a0966c860f
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-10-09 22:58:22 +0545

    init with global styles and added auth route layout  with it's styling

---
+++ storeit/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-amber-100">
+      <h1 className="text-brand text-3xl font-bold ">
+        StoreIt - The only storage solution you need.
+      </h1>
+    </div>
+  );
+}
---
+++ storeit/src/components/AuthForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+type AuthFormType = "sign-in" | "sign-up";
+const AuthForm = ({ type }: { type: AuthFormType }) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
